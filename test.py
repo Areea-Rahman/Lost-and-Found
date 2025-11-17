ba6d43a85a537e08eeffaa76556ee86bcb4f94e9
Linh Tran <112348820+linh-tran-thao@users.noreply.github.com>
2025-11-16 21:19:56 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # =======================
 # LOST & FOUND INTAKE SYSTEM
 # =======================
-from google import genai
+pip install google-genai
 import streamlit as st
 import sqlite3, json, re
 from datetime import datetime, timezone
@@ -401,4 +401,5 @@ if page == "Report Lost Item (User)":
             else:
                 add_lost_item(final_json["description"], contact, email, json.dumps(final_json))
                 st.success("Lost item report submitted successfully!")
+
 
